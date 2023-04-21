ab0e856881bb7bcd53b6920de23ff0bb72b0445d
bettyabnet <betelhemabnet1@gmail.com>
2023-04-21 22:40:40 +0300

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,18 +16,18 @@ int main(void)
 		for (j = i + 1; j < 57; j++)
 		{
 			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
 				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-					if (i != 55 || j != 56 || k != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
-		}			
-		putchar('\n');
-		return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
