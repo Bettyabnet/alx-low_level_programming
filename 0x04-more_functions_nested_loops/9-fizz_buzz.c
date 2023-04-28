b7d61fbba70e9fb58dfbe174afdec9073942587b
bettyabnet <betelhemabnet1@gmail.com>
2023-04-28 22:59:04 +0300

    go

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,9 @@
  * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
 **/
+
 int main(void)
+
 {
 	int i;
 
@@ -15,19 +17,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf(" Fizz ");
+			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz ");
+			printf(" Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz ");
+			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
 		} else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
