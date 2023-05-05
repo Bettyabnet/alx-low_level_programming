27938ef3fb161191fb830b153517a3f77f0bb9dc
bettyabnet <betelhemabnet1@gmail.com>
2023-05-05 08:22:59 +0300

    concatinate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,5 +2,26 @@
 /**
  *  _strcat - concatenates two strings
  *  @src: input value
- *  @dest: input value
- *
+ *  @dest: input value 
+*/
+char *_strcat(char *dest, char *src)
+{
+	int T;
+	int k;
+
+	T = 0
+	while (dest[T] != '\0')
+	{
+		T++;
+	}
+	K = 0;
+	while (src[K] != '\0')
+	{
+		dest[T] = src[K];
+		K++;
+		K++;
+	}
+
+	dest[T] = '\0';
+	return (dest);
+}
