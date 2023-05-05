9eb85993eac601b1358e52348e2be9aac9110d2a
bettyabnet <betelhemabnet1@gmail.com>
2023-05-05 09:06:57 +0300

    not bad

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  _strcat - concatenates two strings
- *  @src: input value
- *  @dest: input value
+ * _strcat - concatenates two strings
+ * @src: input value
+ * @dest: input value
 */
 char *_strcat(char *dest, char *src)
 {
