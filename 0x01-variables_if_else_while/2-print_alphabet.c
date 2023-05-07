d6b38fa28da87c00d25fbf4306a4caac6ef6b73a
bettyabnet <betelhemabnet1@gmail.com>
2023-05-07 23:38:54 +0300

    nice

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
- * * main - Prints the alphabet.
- * *
- * * Return: Always 0 (Success)
- * **/
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[i]);
+		putchar(i);;
 	}
 	putchar('\n');
 	return (0);
