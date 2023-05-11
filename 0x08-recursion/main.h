5ccadb1e7fed49ff65ad517e51c4968ca2bc9cfc
bettyabnet <betelhemabnet1@gmail.com>
2023-05-11 17:15:29 +0300

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
+#include <stdio.h>
 
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,4 +12,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 #endif
