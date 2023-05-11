1c59bc9ddf3b6020d948c47dd06ff739f4247934
bettyabnet <betelhemabnet1@gmail.com>
2023-05-11 22:26:46 +0300

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/*
+ * int factorial - returns a factorial of a given number
+ *
+ * return: always zero
+*/
+int factorial(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else if  (n == 0)
+		return (1);
+
+	{
+		return (n * factorial(n - 1));
+	}
+	_putchar ('\n');
+}
