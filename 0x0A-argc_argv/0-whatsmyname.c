9be0a24d45100e630c795ee2e62d8ad96003de99
bettyabnet <betelhemabnet1@gmail.com>
2023-05-12 14:13:01 +0300

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argc: counts an argument passed on the program
  * @argv: an array of strings
  *
- * return: 0.
+ * return: always 0 (success)
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
