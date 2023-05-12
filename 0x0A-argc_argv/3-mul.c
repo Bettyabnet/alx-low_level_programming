70d3f73ab99f8453eeb961fc4e08ae97c2bb5cb6
bettyabnet <betelhemabnet1@gmail.com>
2023-05-12 15:33:55 +0300

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * atoi: converts string into int
+ *
+ * return: always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+
+	if (i, j, k) != int
+	{
+		printf ("Error\n");
+		return (1);
+	}
+
+	else if (k == i * j)
+	{
+		printf("%s\n", argv[k]);
+		return (0);
+	}
+}
