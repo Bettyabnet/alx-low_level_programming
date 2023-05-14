087af5e8132a6b90d210e171caa8c9abab2550bb
bettyabnet <betelhemabnet1@gmail.com>
2023-05-14 14:07:35 +0300

    advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - function that adds two numbers.
+ * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
  * @r: buffer for result
@@ -18,11 +18,11 @@ for (b = 0; n2[b]; b++)
 if (a > size_r && b > size_r)
 return (0);
 e = 0;
-for (a -= 1, b -= 1, c = 0; c < size_r -1; a--, b--, c++)
+for (a -= 1, b -= 1, c = 0; c < size_r - 1; a--, b--, c++)
 {
 n = e;
-if (a >= 0) 
-n += n1[a] - '0';	
+if (a >= 0)
+n += n1[a] - '0';
 if (b >= 0)
 n += n2[b] - '0';
 if (a < 0 && b < 0 && n == 0)
@@ -36,7 +36,7 @@ r[c] = '\0';
 if (a >= 0 || b >= 0 || e)
 return (0);
 for (c -= 1, d = 0; d < c; c--, d++)
-{	
+{
 e = r[c];
 r[c] = r [d];
 r[d] = e;
