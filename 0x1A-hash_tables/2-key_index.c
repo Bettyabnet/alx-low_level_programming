5c00374a3492c0a307717192344143d052239446
root <root@BettysHp>
2023-10-30 20:40:54 +0300

    so

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,15 @@
-unsigned long int key_index(const unsigned char *key, unsigned long int size) 
-{
-    unsigned long int hash = hash_djb2(key);
-    unsigned long int index = hash % size;
+#include "hash_tables.h"
 
-    return index;
+/**
+ * key_index - A function that returns the normalized index for a key.
+ * @key: The key to get/set in the has table.
+ * @size: the size of the hash table
+ * Return: Normalized index.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = 0;
+
+	index = hash_djb2((unsigned char *)key);
+	return (index % size);
 }
